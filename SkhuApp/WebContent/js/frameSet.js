ce098472559e4e8ebc2501a2855db4c1c134d6ca
ljs5489 <LG@asnir>
2015-03-21 22:04:13 +0900

    메인화면 만들었고, 헤더에 select만들었고, 해당 기사 누르면 왼쪽에 panel이 슬라이드로 떠서 자세한 기사 내용 보여준다.

---
+++ SkhuApp/WebContent/js/frameSet.js
@@ -0,0 +1,13 @@
+/*html태그의 크기를 세밀히 조절하는 javascript모음.*/
+
+$(function(){	
+	$(".ui-select").css("position","absolute");
+	$(".ui-select").css("right","0");
+	$(".ui-select").css("top","0");
+	$(".ui-select").css("margin","3px");
+	
+	
+	//$("#mainScreen").css("height",window.innerHeight-44);
+	
+	
+});
